85ceda17a64a214c5e0a0c41ac669b0477271832
Phil Brookes <phil.brookes@gmail.com>
2019-01-10 19:18:52 +0000

    add deploy template

--- database.sql
+++ database.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS `registrations` (
   `email` varchar(128) NOT NULL,
   `registration_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
